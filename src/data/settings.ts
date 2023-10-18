16d53bc0c795268936ed51f68380aed98099f04a
Mathias Tang <tilmathiastang@gmail.com>
2023-10-18 21:46:33 +0200

    blank operner

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -4,5 +4,4 @@ export const settings = {
   title: "Sminketeatret",
   description:
     "Vi har været aktive på markedet i mere end 20 år, startede med en lille enkelt mands butik i Zoologisk have. Vi opererer nu i hele landet, og har over 130 af danmarks aller dygtigste ansigtsmalere.\n\nLigegyldigt hvor du befinder dig i Danmark har vi mulighed for at komme til netop jeres fest.\n\n“Farver på festen” er vores motto,  der er ikke det arrangement, som vi ikke kan klare.",
-  showLittleSticksPlug: true,
 };
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,10 +3,12 @@ export const socials = [
     name: "Facebook",
     url: "https://www.facebook.com/sminketeatret",
     icon: "mdi:facebook",
+    target: "_blank",
   },
   {
     name: "Instagram",
     url: "https://www.instagram.com/sminketeatret/",
     icon: "mdi:instagram",
+    target: "_blank",
   },
 ];
